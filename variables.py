728dc95114c054c67c5c50b712acb85a994570dd
muturi1803 <nabzmuringo@gmail.com>
2026-02-03 13:40:16 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,17 @@
+  #Variable is container used to store a value
+
+school= "eMobilis"
+firstname = "Taji"
+lastname = "Wilson"
+num1 = 20
+num2 = 148
+language = "Python"
+
+print(school)
+print(firstname)
+print(lastname)
+print(firstname, lastname)
+print(school, "is a coding school")
+print(language,"is a web programming language")
+print(num1+num2)
+print("The sum of",num1 ,"and", num2, "is", num1+num2)
